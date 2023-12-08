64a346f1c64c04e9e5cae80fd035acc782b9e63d
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-08 20:21:52 +0800

    [task/component/TaskSection] Set title to font-semibold

--- app/task/component/TaskSection.tsx
+++ app/task/component/TaskSection.tsx
@@ -2,11 +2,11 @@ import TaskList from "./TaskList";
 
 export default function TaskSection({title}: {title: string}){
     return (
-        <>
+        <div className="my-2">
             <div className="flex flex-col justify-center items-center">
-                <p>{title}</p>
+                <p className="font-semibold">{title}</p>
             </div>
             <TaskList />
-        </>
+        </div>
     )
 }
--- app/task/page.tsx
+++ app/task/page.tsx
@@ -4,7 +4,8 @@ import TaskSection from "./component/TaskSection";
 export default function TaskPage(){
     return (
         <div className="flex flex-col w-full h-screen">
-           <TaskSection title="Hello"/>
+           <TaskSection title="In Progress"/>
+           <TaskSection title="Finished"/>
         </div>
     )
 }
