7271dae9ea0e3081737c815cead4cf5435d72bcc
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-08 20:16:45 +0800

    [api/auth] Add auth endpoint (just for testing)

---
+++ app/api/v1/auth/route.tsx
@@ -0,0 +1,7 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest){
+    return new NextResponse(JSON.stringify({
+        'hello': 'world'
+    }))
+}
