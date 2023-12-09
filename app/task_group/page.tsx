501d1559c5222e9a61f21f70d73c81c3dd9acfc4
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-09 09:56:57 +0800

    [api/task_group] Add task group basic UI

---
+++ app/task_group/page.tsx
@@ -0,0 +1,14 @@
+export default function TaskGroupPage(){
+    return (
+        <div className="flex flex-col bg-orange-300 w-full h-screen justify-center">
+            <div className="flex flex-col justify-center items-center self-center border p-2 m-2 bg-slate-200">
+                <div>
+                    <p className="font-semibold">Proyek</p>
+                </div>
+                <div className="border">
+                    <p>Tugas 1</p>
+                </div>
+            </div>
+        </div>
+    )
+}
