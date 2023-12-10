0ead17abb2228d5a806ca13d4af5695df9504134
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-10 20:04:18 +0800

    [task_group/[id]] Set task based on hardcoded dummy data

--- app/task_group/[id]/component/TaskList.tsx
+++ app/task_group/[id]/component/TaskList.tsx
@@ -18,6 +18,12 @@ export function TaskRow({title, assignedPerson, status, comment}: {title: string
 }
 
 export default function TaskList(){
+    const taskData = [{
+        title: "task 1",
+        assignedPerson: ["Hello"],
+        status: "active",
+        comment: null
+    }]
     return (
        <div className="flex flex-col p-2">
             <div className="flex flex-col relative">
@@ -28,18 +34,17 @@ export default function TaskList(){
                     <p className="flex-1">Status</p>
                     <p className="flex-1">Comment</p>
                 </div>
-                <TaskRow
-                    title="Task 1"
-                    assignedPerson={["hello"]}
-                    status="active"
-                    comment={null}
-                />
-                <TaskRow
-                    title="Task 1"
-                    assignedPerson={["hello"]}
-                    status="active"
-                    comment={null}
-                />
+                {
+                    taskData.map((value, index)=> (
+                        <TaskRow
+                            key={index}
+                            title={value.title}
+                            assignedPerson={value.assignedPerson}
+                            status={value.status}
+                            comment={value.comment}
+                        />
+                    ))
+                }
             </div>
             <div className="bottom-0 bg-orange-200 my-1">
                 <button className="p-1">+</button>
