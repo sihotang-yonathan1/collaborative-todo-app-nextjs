2679623ff2a60406c863dc89a0062de4161b766e
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-10 20:28:00 +0800

    [task_group] Set project based on data in database

--- app/task_group/page.tsx
+++ app/task_group/page.tsx
@@ -1,6 +1,20 @@
+import { cookies } from "next/headers";
 import TaskGroupPreview from "./component/TaskGroupPreview";
+import { getProyekByUsername } from "../api/v1/proyek/proyek";
 
-export default function TaskGroupPage(){
+type ProyekType = {
+    id: number,
+    title: string,
+    username: string | null
+}
+
+export default async function TaskGroupPage(){
+    const currentCookie = cookies()
+    const username = currentCookie.get("username")?.value
+    let proyekList: ProyekType[] = []
+    if (username !== undefined){
+        proyekList = await getProyekByUsername(username)
+    }
     return (
         <div className="flex flex-col bg-orange-300 w-full h-screen justify-center">
             <div className="flex flex-col justify-center items-center self-center border p-2 m-2 bg-slate-200">
@@ -8,14 +22,10 @@ export default function TaskGroupPage(){
                     <p className="font-semibold">Proyek</p>
                 </div>
                 <div className="grid grid-cols-5">
-                    <TaskGroupPreview title="world" id={1} />
-                    <TaskGroupPreview title="world" id={1} />
-                    <TaskGroupPreview title="world" id={1} />
-                    <TaskGroupPreview title="world" id={1} />
-                    <TaskGroupPreview title="world" id={1} />
-                    <TaskGroupPreview title="world" id={1} />
-                    <TaskGroupPreview title="world" id={1} />
-                    <TaskGroupPreview title="world" id={1} />
+                    { proyekList.map((value) => (
+                        <TaskGroupPreview id={value.id} key={value.id} title={value.title} />
+                    ))}
+                    
                 </div>
             </div>
         </div>
