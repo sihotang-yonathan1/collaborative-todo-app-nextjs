8107cdda445b6a69382a7fa94419a92966947e07
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-11 16:45:14 +0800

    [task_group/[id]/page] Add header and back button

--- app/task_group/[id]/page.tsx
+++ app/task_group/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import TaskList from "./component/TaskList";
 import TaskSection from "./component/TaskSection";
 
@@ -5,6 +6,14 @@ export default function TaskPage({params}: {params: {id: number}}){
     const tugasListId = params.id ?? -1
     return (
         <div className="flex flex-col w-full h-screen">
+            <div className="flex bg-sky-300 relative">
+                <Link href="/task_group" className="justify-self-start bg-slate-500 px-2">
+                    <button>Back</button>
+                </Link>
+                <div className="absolute left-1/2">
+                    <p className="font-semibold">List Tugas</p>
+                </div>
+            </div>
            <TaskSection title="In Progress" tugasListId={tugasListId} status="in_progress"/>
            <TaskSection title="Finished" tugasListId={tugasListId} status="Finished"/>
         </div>
