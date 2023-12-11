f2ed3957d088cdc4e70a5749ce13322c7656e586
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-11 21:15:29 +0800

    [task_group] Add layout tsx as the intermediate component to make TaskGroup possible to use client component

--- app/task_group/page.tsx
+++ app/task_group/page.tsx
@@ -1,22 +1,14 @@
-import { cookies } from "next/headers";
 import TaskGroupPreview from "./component/TaskGroupPreview";
-import { getAllProyek} from "../api/v1/proyek/proyek";
 
 type ProyekType = {
     id: number,
     title: string,
 }
 
-export default async function TaskGroupPage(){
-    const currentCookie = cookies()
-    const username = currentCookie.get("username")?.value
-    let proyekList: ProyekType[] = []
-    if (username !== undefined){
-        proyekList = await getAllProyek()
-    }
+export default async function TaskGroupPage({proyekList}: {proyekList: ProyekType[]}){
     return (
-        <div className="flex flex-col bg-orange-300 w-full h-screen justify-center">
-            <div className="flex flex-col justify-center items-center self-center border p-2 m-2 bg-slate-200">
+        <div className="flex flex-col bg-orange-300 w-full h-screen justify-center relative">
+            <div className="flex flex-col justify-center items-center self-center border p-2 m-2 bg-slate-200 relative">
                 <div>
                     <p className="font-semibold">Proyek</p>
                 </div>
@@ -25,6 +17,22 @@ export default async function TaskGroupPage(){
                         <TaskGroupPreview id={value.id} key={value.id} title={value.title} />
                     ))}
                     
+                </div>
+                <div className="absolute bottom-0 right-0 py-1 bg-sky-300 rounded-full px-3 m-1">
+                    <button>+</button>
+                </div>
+
+                <div className="absolute bg-slate-400 z-10 flex flex-col p-2 left-1/4">
+                    <div className="flex justify-center">
+                        <p className="font-semibold">Add Project</p>
+                    </div>
+                    <div className="flex justify-center my-2">
+                        <input type="text" name="project_title" id="project_title" placeholder="Project Name"/>
+                    </div>
+                    <div className="flex justify-between">
+                        <button>Add</button>
+                        <button>Cancel</button>
+                    </div>
                 </div>
             </div>
         </div>
