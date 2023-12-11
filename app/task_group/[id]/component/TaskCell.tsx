132edc7ca65613c4cb7f416a348df64db34da949
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-11 15:26:43 +0800

    [TaskCell] Set TaskCell event to tempTaskList

--- app/task_group/[id]/component/TaskCell.tsx
+++ app/task_group/[id]/component/TaskCell.tsx
@@ -4,8 +4,8 @@ export default function TaskCell({ data, onEdit, taskKey, isEditMode = false}:
     { data: string | null, isEditMode: boolean , taskKey: string, onEdit: (key: string, value: string) => void}) {
     return (
         <td className="text-center border-b border-l border-t">
-            <div contentEditable={isEditMode} suppressContentEditableWarning={true}>
-                {data}
+            <div contentEditable={isEditMode} suppressContentEditableWarning={true} onInput={e => onEdit(taskKey, e.currentTarget.textContent ?? "")}>
+                {data ?? ""}
             </div>
         </td>
     );
