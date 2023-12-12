ca57814d45c08aadc67680e6fede66df0c526818
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-12 12:56:14 +0800

    [UI/login] Set background image to fit the screen

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -21,36 +21,38 @@ export default function LoginPage(){
     
     }
     return (
-        <div className="flex flex-col justify-center h-screen items-center bg-bg-login">
+        <div className="flex flex-col justify-center h-screen items-center bg-bg-login bg-cover">
             <form className="bg-transparent flex flex-col p-2" action={checkLoginWithFormData}>
                 <h3 className="font-semibold self-center text-white text-xl">Welcome Back</h3>
-                <div className="w-[40vw] mx-3 my-3">
+                <div className="mx-3 my-3">
                     <p className="flex flex-wrap text-white text-sm">Bersama BifiMerah, Tugas Terlihat Lebih Ringan</p>
                 </div>
-                <div>
-                    <div className="text-white px-3">
-                        <label htmlFor="username" className="text-white text-sm">Username</label>
-                    </div>
+                <div className="px-1 flex flex-col items-center">
                     <div>
-                        <input 
-                            type="text" 
-                            name="username" 
-                            id="username" 
-                            className="bg-white p-2 m-2 rounded-xl"
-                        />
-                    </div>
-                </div>
-                <div>
-                    <div className="px-3">
-                        <label htmlFor="password" className="capitalize font-light text-white text-sm">password</label>
-                    </div>
-                    <div>
-                        <input 
-                            type="password" 
-                            name="password" 
-                            id="password"
-                            className="bg-white p-2 m-2 rounded-xl"
+                        <div className="text-white px-3">
+                            <label htmlFor="username" className="text-white text-sm">Username</label>
+                        </div>
+                        <div>
+                            <input 
+                                type="text" 
+                                name="username" 
+                                id="username" 
+                                className="bg-white p-2 m-2 rounded-xl"
                             />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="px-3">
+                            <label htmlFor="password" className="capitalize font-light text-white text-sm">password</label>
+                        </div>
+                        <div>
+                            <input 
+                                type="password" 
+                                name="password" 
+                                id="password"
+                                className="bg-white p-2 m-2 rounded-xl"
+                                />
+                        </div>
                     </div>
                 </div>
                 <div className="flex my-5 py-5">
