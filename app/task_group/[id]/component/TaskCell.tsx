30bba51091b110bc6a01f20a2277ec7947ca468d
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-12 22:13:37 +0800

    [TAskCell] Add support for adding support to hold newline as well

--- app/task_group/[id]/component/TaskCell.tsx
+++ app/task_group/[id]/component/TaskCell.tsx
@@ -6,7 +6,7 @@ export default function TaskCell({ data, onEdit, taskKey, isEditMode = false}:
     { data: string | null, isEditMode: boolean , taskKey: string, onEdit: (key: string, value: string) => void}) {
     return (
         <TaskContainer>
-            <div contentEditable={isEditMode} suppressContentEditableWarning={true} onInput={e => onEdit(taskKey, e.currentTarget.textContent ?? "")}>
+            <div contentEditable={isEditMode} suppressContentEditableWarning={true} onInput={e => onEdit(taskKey, e.currentTarget.innerHTML ?? "")}>
                 {data ?? ""}
             </div>
         </TaskContainer>
