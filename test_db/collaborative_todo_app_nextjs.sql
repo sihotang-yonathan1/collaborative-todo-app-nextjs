825775ab5a7aef82e25beff9d5d834885ec1d690
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-13 20:35:08 +0800

    [test_db] Add new version of new table schema

--- test_db/collaborative_todo_app_nextjs.sql
+++ test_db/collaborative_todo_app_nextjs.sql
@@ -58,10 +58,11 @@ CREATE TABLE `tugas` (
   `comment` varchar(255) DEFAULT NULL,
   `tugas_list_id` int(11) NOT NULL,
   `priority_level` int(11) DEFAULT NULL,
+  `is_client_accepted` tinyint(1) DEFAULT 0,
   PRIMARY KEY (`id`),
   KEY `tugas_list_id` (`tugas_list_id`),
   CONSTRAINT `tugas_ibfk_1` FOREIGN KEY (`tugas_list_id`) REFERENCES `tugas_list` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -71,9 +72,10 @@ CREATE TABLE `tugas` (
 LOCK TABLES `tugas` WRITE;
 /*!40000 ALTER TABLE `tugas` DISABLE KEYS */;
 INSERT INTO `tugas` VALUES
-(1,'First Task','in_progress','Kurang 1 lagi',1,1),
-(6,'title 12','in_progress','comment',1,3),
-(7,'title 1','in_progress','comment',1,2);
+(1,'First Task','in_progress','Kurang 1 lagi',1,1,NULL),
+(6,'title 12','in_progress','comment',1,2,NULL),
+(7,'title 1','review','comment',1,3,NULL),
+(9,'title','in_progress','comment',1,9999,NULL);
 /*!40000 ALTER TABLE `tugas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -101,8 +103,40 @@ CREATE TABLE `tugas_assign` (
 LOCK TABLES `tugas_assign` WRITE;
 /*!40000 ALTER TABLE `tugas_assign` DISABLE KEYS */;
 INSERT INTO `tugas_assign` VALUES
-(1,'user');
+(7,'admin');
 /*!40000 ALTER TABLE `tugas_assign` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tugas_comment`
+--
+
+DROP TABLE IF EXISTS `tugas_comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tugas_comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tugas_id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `content` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tugas_id` (`tugas_id`),
+  KEY `username` (`username`),
+  CONSTRAINT `tugas_comment_ibfk_1` FOREIGN KEY (`tugas_id`) REFERENCES `tugas` (`id`),
+  CONSTRAINT `tugas_comment_ibfk_2` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tugas_comment`
+--
+
+LOCK TABLES `tugas_comment` WRITE;
+/*!40000 ALTER TABLE `tugas_comment` DISABLE KEYS */;
+INSERT INTO `tugas_comment` VALUES
+(1,1,'admin','Hello World'),
+(2,1,'user','Hello World 23');
+/*!40000 ALTER TABLE `tugas_comment` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -157,6 +191,7 @@ LOCK TABLES `user` WRITE;
 INSERT INTO `user` VALUES
 ('admin','admin','admin'),
 ('manager','manager','manager'),
+('new worker','12345678','worker'),
 ('user','user','user'),
 ('worker','worker','worker');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
@@ -171,4 +206,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-12-12 17:49:22
+-- Dump completed on 2023-12-13 20:33:49
