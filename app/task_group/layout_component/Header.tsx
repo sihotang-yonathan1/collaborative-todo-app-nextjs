2402493be04f966dac2e1b825bdfc0f86c53354e
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-12-13 21:32:36 +0800

    [UI/Header] Add icon to header

--- app/task_group/layout_component/Header.tsx
+++ app/task_group/layout_component/Header.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import BifiMerahLogo from "@/assets/bifi_merah_logo.png";
 
+import { FiUser, FiSearch } from "react-icons/fi";
+
 export default function Header(){
     return (
         <div className="p-2 bg-[#3a7782] grid-cols-6 grid gap-3">
@@ -12,17 +14,24 @@ export default function Header(){
                 />
             </div>
             <div className="grid col-start-5 col-end-5 my-3">
-                <input 
-                    type="text" 
-                    name="searchbar" 
-                    id="searchbar" 
-                    placeholder="Find your project"
-                    className="w-[28vw] rounded-md px-2 py-1 bg-[#eed8dc]"
-                />
+                <div className="flex bg-[#eed8dc]">
+                    <div className="flex items-center">
+                        <FiSearch />
+                    </div>
+                    <div>
+                        <input 
+                            type="text" 
+                            name="searchbar" 
+                            id="searchbar" 
+                            placeholder="Find your project"
+                            className="w-[28vw] rounded-md px-2 py-1 bg-[#eed8dc]"
+                        />
+                    </div>
+                </div>
             </div>
             <div className="col-start-7 col-end-7">
-                <div className="h-full flex items-center rounded-full bg-gray-500">
-                    <p>Avatar</p>
+                <div className="h-full flex items-center rounded-full bg-gray-500 px-4">
+                    <FiUser/>
                 </div>
             </div>
         </div>
